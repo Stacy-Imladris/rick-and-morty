6fb530a2306980ff3c4a4515ef22581f946ba64d
stacy <ada.davis27@gmail.com>
2022-05-24 23:40:25 +0300

    changes

--- src/components/CharacterContent/CharacterContent.tsx
+++ src/components/CharacterContent/CharacterContent.tsx
@@ -5,10 +5,11 @@ import s from './CharacterContent.module.scss'
 import {Character} from './Character/Character';
 import {Paginator} from './Paginator/Paginator';
 import {Search} from './Search/Search';
+import {selectCharacters} from '../../store/selectors';
 
 export const CharacterContent = () => {
     const dispatch = useAppDispatch()
-    const characters = useAppSelector(state => state.characters.characters)
+    const characters = useAppSelector(selectCharacters)
 
     useEffect(() => {
         dispatch(getCharacters())
---
+++ src/store/selectors.ts
@@ -0,0 +1,3 @@
+import {RootState} from './store';
+
+export const selectCharacters = (state: RootState) => state.characters.characters
