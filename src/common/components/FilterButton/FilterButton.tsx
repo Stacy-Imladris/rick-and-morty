23da09c779019c944bfb1405200e3f83956e0c7f
stacy <ada.davis27@gmail.com>
2022-05-24 23:21:50 +0300

    changes

--- src/common/components/FilterButton/FilterButton.tsx
+++ src/common/components/FilterButton/FilterButton.tsx
@@ -1,3 +1,7 @@
-export const FilterButton = () => {
-    return <button>female</button>
+type FilterButtonPropsType = {
+    title: string
+}
+
+export const FilterButton = ({title}: FilterButtonPropsType) => {
+    return <button>{title}</button>
 }
